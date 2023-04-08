b980d844867eff06d4ec111d875f304179124701
Adedamola Adeyemi <Adedamolaiadeyemi@gmail.com>
2023-04-08 08:10:59 +0100

    Update MainActivity.cs

--- App01/App01.Android/MainActivity.cs
+++ App01/App01.Android/MainActivity.cs
@@ -12,7 +12,7 @@ namespace App01.Droid
     [Activity(Label = "App01", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle savedInstanceState) 
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
